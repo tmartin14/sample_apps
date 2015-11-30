bf87f4aebc28fb2acc948d727b72e122c23d9ff1
tmartin14 <tmartin14@gmail.com>
2015-11-29 19:53:19 -0500

    Updated to add New Relic settings
    When running in CF, you’ll need to add 2 additional environment
    variables:
    
    NEW_RELIC_APP_NAME
    NEW_RELIC_LICENSE_KEY

--- app_python/helloWorld.py
+++ app_python/helloWorld.py
@@ -1,12 +1,19 @@
 import os
 from flask import Flask
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-	return '<h1>Hello World!</h1>  <br/><br/>This is a Python app running on Cloud Foundry.'
+    return '<h1>Hello World!</h1>  <br/>This is a Python app running on Cloud Foundry.<br/><br/><br/><b>Time:</b>  %s' % datetime.now().time()
 
 port = os.getenv('PORT', '5000')
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=int(port))
+    
+
+    
+#use 2 envrionment variables to add New Relic:
+#  NEW_RELIC_APP_NAME
+#  NEW_RELIC_LICENSE_KEY 
