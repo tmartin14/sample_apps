0ccecf9d0849ee12d9282b591ac8ce563152a503
Tom Martin <tmartin14@gmail.com>
2017-03-13 13:18:41 -0400

    adding new relic and testing Jenkins

--- app_node/helloWorld.js
+++ app_node/helloWorld.js
@@ -8,7 +8,7 @@ app.use(express.static(__dirname + '/public'));
 
 app.get("/", function (request, response) {
     response.writeHead(200, {"Content-Type": "text/html"})
-    response.end("<h1>Hello World!</h1>  <br/>This is a <b>Node.js</b> app running on Cloud Foundry.<br/><br/><br/><b>Time:</b> " + new Date().toUTCString())
+    response.end("<h1>Hello World!</h1>  <br/>This is a <b>Node.js</b> application.<br/><br/><b>Time:</b> " + new Date().toUTCString())
 });
 
 app.listen(port);
